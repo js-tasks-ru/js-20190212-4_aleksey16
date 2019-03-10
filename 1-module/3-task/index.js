d7ec791d63d26f0972ede0870d607186ecaf3d7b
a.alexeenko <a.alexeenko@qiwi.com>
2019-03-10 13:44:32 +0300

    2.1

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,24 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
 function getMinMax(str) {
+    let strArr, res, i, parseFl;
+
+    strArr = str.split(" ");
+    strArr = strArr.join().split(',');
+    res = [];
+
+    for ( i = 0; i < strArr.length; ++i) {
+        parseFl = parseFloat(strArr[i]);
+        if (!isNaN(parseFl && isFinite(strArr[i])))
+            res.push(parseFl);
+    }
+    res.sort(function (a, b) {
+        if (a < b) return -1;
+        if (a > b) return 1;
+    });
+    return {min: res[0], max: res[res.length-1]};
 
 }
 
